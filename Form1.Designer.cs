d461d6bf0386854eb9ac6b48dcfcbbed5e817112
webmarcelo88 <52475611+webmarcelo88@users.noreply.github.com>
2019-07-14 20:56:22 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ExemploPJe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consultar Processo no PJe";
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.Location = new System.Drawing.Point(542, 51);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(138, 23);
+            this.btnTestar.TabIndex = 8;
+            this.btnTestar.Text = "TESTAR SERVIÇO";
+            this.btnTestar.UseVisualStyleBackColor = true;
+            this.btnTestar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(402, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Os dados de acesso e configurações são informados via código fonte de forma fixa." +
+    "";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(18, 116);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(712, 326);
+            this.txtResult.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 454);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PJe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using ExemploPJe.PJe;
+using System;
+using System.Windows.Forms;
+
+namespace ExemploPJe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Testar();
+            //Testar_PJE_TJMT();
+        }
+
+        private void Testar()
+        {
+            string idConsultante = "75293730215";
+            string senhaConsultante = "admin123";
+            string numeroProcesso = "1002111-71.2018.8.11.0051";
+
+            string dataReferencia = "";
+            bool movimentos = false;
+            bool incluirCabecalho = false;
+            bool incluirDocumentos = false;
+            tipoDocumento documento = null;
+
+            string mensagem;
+            tipoProcessoJudicial tipoProcesso;
+
+            try
+            {
+
+                servicointercomunicacao222Client client = new servicointercomunicacao222Client();
+                var result = client.consultarProcesso(idConsultante,
+                                                      senhaConsultante,
+                                                      numeroProcesso,
+                                                      dataReferencia,
+                                                      movimentos,
+                                                      incluirCabecalho,
+                                                      incluirDocumentos,
+                                                      documento,
+                                                      out mensagem,
+                                                      out tipoProcesso);
+
+                //string auxMensagem = ((result) ? "Consulta de processo executada com sucesso. " : "Erro ao executar consulta de processo");
+                //MessageBox.Show($"{auxMensagem}{Environment.NewLine}{mensagem}{Environment.NewLine}Processo: {tipoProcesso.dadosBasicos.numero}");
+
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message;
+            }
+        }
+
+        private void Testar_PJE_TJMT()
+        {
+            string idConsultante = "";
+            string senhaConsultante = "";
+            string numeroProcesso = "1002111-71.2018.8.11.0051";
+
+            string dataReferencia = "";
+            bool movimentos = false;
+            bool incluirCabecalho = false;
+            bool incluirDocumentos = false;
+            string[] documento = null;
+
+            string mensagem;
+            PJeTMT.tipoProcessoJudicial tipoProcesso;
+
+            try
+            {
+
+                PJeTMT.servicointercomunicacao222Client client = new PJeTMT.servicointercomunicacao222Client();
+                var result = client.consultarProcesso(idConsultante,
+                                                      senhaConsultante,
+                                                      numeroProcesso,
+                                                      dataReferencia,
+                                                      movimentos,
+                                                      incluirCabecalho,
+                                                      incluirDocumentos,
+                                                      documento,
+                                                      out mensagem,
+                                                      out tipoProcesso);
+
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message;
+            }
+        }
+    }
+}
